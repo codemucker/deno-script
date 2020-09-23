563aca38e79584863d1e99aed60d71a27170668f
Bert van Brakel <bertvanbrakel@gmail.com>
2020-09-23 10:57:44 +1000

    remove duplicate function

--- util.ts
+++ util.ts
@@ -46,16 +46,3 @@ export function extractFunctionDocs(func: Function): string | undefined {
     }
     return undefined
 }
-
-export function printTasks(lines: string[], tasks: { [name: string]: any }) {
-    Object.keys(tasks)
-        .sort()
-        .forEach((key) => {
-            let docs = extractFunctionDocs(tasks[key]) || ''
-            let name = key
-            if (name.startsWith('task_')) {
-                name = name.substring(5)
-            }
-            lines.push(`     ${rightPad(name, 25)} : ${docs}`)
-        })
-}
